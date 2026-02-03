b3e665f1bb5b2bc8780aa72fbfc3ea26480b254d
Hwangseoeun <sunny030921@gmail.com>
2026-02-03 16:38:20 +0900

    feat: [섹션4. 스프링 핵심 원리 이해2 - 객체지향 원리 적용] 순수 자바 코드에서 스프링으로 전환

--- src/main/java/springCorePrinciples/basic/AppConfig.java
+++ src/main/java/springCorePrinciples/basic/AppConfig.java
@@ -1,5 +1,7 @@
 package springCorePrinciples.basic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springCorePrinciples.basic.discount.DiscountPolicy;
 import springCorePrinciples.basic.discount.RateDiscountPolicy;
 import springCorePrinciples.basic.member.MemberRepository;
@@ -9,21 +11,26 @@ import springCorePrinciples.basic.member.MemoryMemberRepository;
 import springCorePrinciples.basic.order.OrderService;
 import springCorePrinciples.basic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 }
--- src/main/java/springCorePrinciples/basic/OrderApplication.java
+++ src/main/java/springCorePrinciples/basic/OrderApplication.java
@@ -1,5 +1,7 @@
 package springCorePrinciples.basic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springCorePrinciples.basic.member.Grade;
 import springCorePrinciples.basic.member.Member;
 import springCorePrinciples.basic.member.MemberService;
@@ -8,9 +10,13 @@ import springCorePrinciples.basic.order.OrderService;
 
 public class OrderApplication {
     public static void main(String[] args) {
-        final AppConfig appConfig = new AppConfig();
+        /*final AppConfig appConfig = new AppConfig();
         final MemberService memberService = appConfig.memberService();
-        final OrderService orderService = appConfig.orderService();
+        final OrderService orderService = appConfig.orderService();*/
+
+        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        final MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        final OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         final Long memberId = 1L;
         final Member member = new Member(memberId, "memberA", Grade.VIP);
