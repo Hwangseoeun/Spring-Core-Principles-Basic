69952abdfa3652f02ced04382f3e1237dad9aad6
Hwangseoeun <sunny030921@gmail.com>
2026-02-08 00:27:08 +0900

    feat: [섹션7. 컴포넌트 스캔] ComponentScan에 대한 추가 설명 주석 추가

--- src/main/java/springCorePrinciples/basic/AutoAppConfig.java
+++ src/main/java/springCorePrinciples/basic/AutoAppConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+
+// ComponentScan 내부에 basePackages와 같은 탐색할 패키지의 시작 위치를 별도로 지정하지 않으면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 됨
 @Configuration
 @ComponentScan(
+//    basePackages = "springCorePrinciples.basic.member",
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
