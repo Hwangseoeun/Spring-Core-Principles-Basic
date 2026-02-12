cfccd38834d27b61e16c1a47f92960fdcfd5fa65
Hwangseoeun <sunny030921@gmail.com>
2026-02-12 14:12:31 +0900

    feat: [섹션8. 의존관계 자동 주입] lombok 라이브러리를 통해 생성자 코드 생략

--- src/main/java/springCorePrinciples/basic/order/OrderServiceImpl.java
+++ src/main/java/springCorePrinciples/basic/order/OrderServiceImpl.java
@@ -1,20 +1,17 @@
 package springCorePrinciples.basic.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import springCorePrinciples.basic.discount.DiscountPolicy;
 import springCorePrinciples.basic.member.Member;
 import springCorePrinciples.basic.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(final MemberRepository memberRepository, final DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
